869348f8c16a28f91fdc84099a2535f62fc61945
emrald-dev <muriithi.wanderi.daniel@gmail.com>
2026-01-04 23:36:25 +0300

    (feat) PACKAGING: Add build and installation scripts

--- main.py
+++ main.py
@@ -11,6 +11,11 @@ app = typer.Typer(help="My Linux CLI helper")
 def ping():
     fs.ping_folders()
 
+@app.command()
+def dev():
+    cons.print("This feature will clone this project and create a workspace.")
+    cons.print("Not implemented yet")
+
 if __name__ == "__main__":
     app()
 
