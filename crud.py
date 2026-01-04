2f88d53050e9baf47fa53da5c661fb1e76de633c
emrald-dev <muriithi.wanderi.daniel@gmail.com>
2026-01-04 22:39:02 +0300

    (feat) PING: Add folder pinging ability to see folders

--- crud.py
+++ crud.py
@@ -1,8 +1,29 @@
 from pathlib import Path
 from rich.console import Console
 
-
-root = Path(__file__).resolve().parent
-deps = root / "deps.yaml"
 cons = Console()
 
+class Logger():
+    def __init__(self):
+        pass
+
+    def ping(self, msg):
+        cons.print(f"[bold green]PING[/bold green]: {msg}")
+
+logger = Logger()
+
+class FileSystem:
+    def __init__(self):
+        self.home = Path(__file__).resolve().home()
+        self.project = Path(__file__).resolve().parent
+        self.deps = self.project / "deps.yaml"
+
+fs = FileSystem()
+
+def update_dotfiles():
+    cons.print("Attempting dotfiles update, please wait.")
+
+    try:
+        pass
+    except:
+        pass
--- main.py
+++ main.py
@@ -8,9 +8,10 @@ app = typer.Typer(help="My Linux CLI helper")
 from crud import *
 
 @app.command()
-def hello():
-    cons.print("Helllo world")
-
+def ping():
+    logger.ping(f"(Home Folder) [cyan]{fs.home}[/cyan]")
+    logger.ping(f"(Project Folder) [cyan]{fs.project}[/cyan]")
+    logger.ping(f"(Deps Folder) [cyan]{fs.deps}[/cyan]")
 
 if __name__ == "__main__":
     app()
